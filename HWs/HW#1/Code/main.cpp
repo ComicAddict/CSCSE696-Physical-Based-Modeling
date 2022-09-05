b23c076b3d374121c7263c25ecb6a2034b001cdf
ComicAddict <tolgatalhayldz@gmail.com>
2022-09-05 12:30:09 -0500

    model transformations and cube primitive added

--- HWs/HW#1/Code/main.cpp
+++ HWs/HW#1/Code/main.cpp
@@ -11,10 +11,27 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+glm::vec3 camPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 camFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 camUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTimeFrame = .0f;
+float lastFrame = .0f;
+
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    float camSpeed = static_cast<float>(2.5 * deltaTimeFrame);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camPos += camSpeed * camFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camPos -= camSpeed * camFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camPos -= camSpeed * glm::normalize(glm::cross(camFront,camUp));
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camPos += camSpeed * glm::normalize(glm::cross(camFront, camUp));
 }
 
 int main() {
@@ -52,8 +69,8 @@ int main() {
 
 
     float vertices[] = {
-        .5f, .5f, .0f,
-        .5f, -.5f, .0f,
+        .5f, .5f, 1.0f,
+        .5f, -.5f, 1.0f,
         -.5f, -.5f, .0f,
         -.5f, .5f, .0f
     };
@@ -62,18 +79,73 @@ int main() {
         0, 1, 3,
         1, 2, 3
     };
+
+    float cubevertices[] = {
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f
+    };
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
+
     unsigned int VBO;
     glGenBuffers(1, &VBO);
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubevertices), cubevertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -81,32 +153,63 @@ int main() {
     glBindVertexArray(0);
 
     Shader primitiveShader("../../../HWs/HW#1/Code/shaders/vertex_shader_prim.glsl","../../../HWs/HW#1/Code/shaders/fragment_shader_prim.glsl");
-    Shader perspective("../../../HWs/HW#1/Code/shaders/vert.glsl", "../../../HWs/HW#1/Code/shaders/frag.glsl");
+    Shader sperspective("../../../HWs/HW#1/Code/shaders/vert.glsl", "../../../HWs/HW#1/Code/shaders/frag.glsl");
 
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    sperspective.use();
 
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -13.0f));
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, .1f, 100.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    //glm::mat4 view = glm::mat4(1.0f);
+    //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
 
     glEnable(GL_DEPTH_TEST);
 
+    //glm::vec3 camPos = glm::vec3(1.0f, 1.0f, 1.0f);
+    //glm::vec3 camTarget = glm::vec3(.0f, .0f, .0f);
+    //glm::vec3 camDir = glm::normalize(camPos - camTarget);
+
+    //glm::vec3 up = glm::vec3(.0f, .0f, 1.0f);
+    //glm::vec3 camRight = glm::normalize(glm::cross(up, camDir));
+    //glm::vec3 camUp = glm::normalize(glm::cross(camRight, camDir));
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)800.0f / (float)600.0f, 0.1f, 100.0f);
+    sperspective.setMat4("projection", projection);
+
     while (!glfwWindowShouldClose(window))
-    {
+    {   
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTimeFrame = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         processInput(window);
+
         glClearColor(.2f, .3f, .3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        int viewLoc = glGetUniformLocation(perspective.ID, "view");
-        int projLoc = glGetUniformLocation(perspective.ID, "projection");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-        perspective.use();
+
+        sperspective.use();
+
+        glm::mat4 view = glm::lookAt(camPos, camPos + camFront, camUp);
+        sperspective.setMat4("view", view);
+
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            sperspective.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
 
