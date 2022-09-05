b290216d615e17db0d4920b14369f596be314d8b
ComicAddict <tolgatalhayldz@gmail.com>
2022-09-04 20:39:13 -0500

    shaders is not used currently, got the first triangle

--- HWs/HW#1/Code/main.cpp
+++ HWs/HW#1/Code/main.cpp
@@ -36,14 +36,92 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    float vertices[] = {
+        -.5f, -.5f, .0f,
+         .5f, -.5f, .0f,
+         .0f,  .5f, .0f
+    };
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    const char *vertexShaderSource = "#version 330 core\n"
+        "layout(location = 0) in vec3 aPos;\n"
+        "void main() {\n"
+            "gl_Position = vec4(aPos, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char errLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, errLog);
+        std::cout << "ERROR: Shader Vertex Compilation Failed\n" << errLog << std::endl;
+    }
+
+    const char *fragShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main(){\n"
+            "FragColor = vec4(1.0f,.5f,.2f,1.0f);\n"
+        "}\0";
+
+    unsigned int fragShader;
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+    glCompileShader(fragShader);
+
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(fragShader, 512, NULL, errLog);
+        std::cout << "ERROR: Shader Fragment Compilation Failed\n" << errLog << std::endl;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, errLog);
+        std::cout << "ERROR: Shader Program Linking Failed\n" << errLog << std::endl;
+    }
+
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
         glClearColor(.2f, .3f, .3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragShader);
 
     glfwTerminate();
 
