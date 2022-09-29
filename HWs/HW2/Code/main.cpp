b36e5dca019602d648a2fae315c063c77c5dd758
ComicAddict <tolgatalhayldz@gmail.com>
2022-09-29 09:12:46 -0500

    last changes before demo

--- HWs/HW2/Code/main.cpp
+++ HWs/HW2/Code/main.cpp
@@ -74,14 +74,8 @@ struct particleGenerator { //for now this is a directional generator, gonna add 
     int pnum = 0;
 };
 
-struct triangle {
-    glm::vec3 v1;
-    glm::vec3 v2;
-    glm::vec3 v3;
-};
-
 struct collider {
-    triangle* triangles;
+    glm::vec3* v;
 };
 
 void generateWireframeCube(float cubeSize, float* vertices) {
@@ -142,6 +136,9 @@ void particleShaderSetup() {
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // color
 }
 
+float hpSign(glm::vec2 p1, glm::vec2 p2, glm::vec2 p3) {
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+}
 
 int main() {
 
@@ -229,9 +226,18 @@ int main() {
     particleShaderSetup();
 
     // add colliders
+    // for not a simple triangle is enough i think :D
+    glm::vec3 tri[3] = {glm::vec3(0.0, 0.0, 0.0),
+                        glm::vec3(0.0, 10.0, 10.0),
+                        glm::vec3(0.0, -10.0, 10.0) };
+    glm::vec3 norm = glm::normalize(glm::cross(tri[0] - tri[1], tri[0] - tri[2]));
+    unsigned int collVao;
+    glGenVertexArrays(1, &collVao);
+    glBindVertexArray(collVao);
+    VertexBuffer colvbo(tri,sizeof(tri));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-    collider coll1;
-    coll1.triangles = new triangle[100];
 
     //trying to render an icosphere
 
@@ -241,10 +247,9 @@ int main() {
     Shader fperspective("../../../HWs/HW#1/Code/shaders/vert_flat.glsl", "../../../HWs/HW#1/Code/shaders/frag_flat.glsl");
     Shader particleShader("../../../HWs/HW2/Code/shaders/vertParticle.glsl", "../../../HWs/HW2/Code/shaders/fragParticle.glsl");
     Shader coneShader("../../../HWs/HW2/Code/shaders/vertCone.glsl", "../../../HWs/HW2/Code/shaders/fragCone.glsl");
+    Shader collShader("../../../HWs/HW2/Code/shaders/vertColl.glsl", "../../../HWs/HW2/Code/shaders/fragColl.glsl");
 
-    sperspective.use();
-
-    float pointSize = 10.f;
+    float pointSize = 4.0f;
     float lineSize = 6.0f;
 
     glPointSize(pointSize);
@@ -334,7 +339,16 @@ int main() {
         coneShader.setMat4("model", model);
         glBindVertexArray(coneVao);
         glDrawArrays(GL_TRIANGLES, 0, 96);
+        
+        collShader.use();
+        collShader.setMat4("view", view);
+        collShader.setMat4("projection", projection);
+        model = glm::mat4(1.0f);
+        collShader.setMat4("model", model);
+        glBindVertexArray(collVao);
+        glDrawArrays(GL_TRIANGLES, 0 , 3);
 
+        glBindVertexArray(0);
         // all drawings done lets do some imgui stuff
 
         ImGui::Begin("Particle Generator Settings");
@@ -405,7 +419,7 @@ int main() {
             pgen2.t += deltaTimeFrame;
             if (pgen1.t > pgen1.P && pgen1.pnum < MAX_PARTICLE_PER_GENERATOR) {
                 pgen1.t = 0.0f;
-                glm::vec3 vgen = pgen1.v + pgen1.d;
+                glm::vec3 vgen = pgen1.v + 3.0f * pgen1.d;
                 glm::vec3 var = glm::vec3(velVariance, velVariance, velVariance);
                 glm::vec3 vgenc = glm::gaussRand(vgen, var-glm::dot(var,vgen));
                 pgen1.pcpus[pgen1.pnum] = {
@@ -425,7 +439,7 @@ int main() {
 
             if (pgen2.t > pgen2.P && pgen2.pnum < MAX_PARTICLE_PER_GENERATOR) {
                 pgen2.t = 0.0f;
-                glm::vec3 vgen = pgen2.v + pgen2.d;
+                glm::vec3 vgen = pgen2.v + 3.0f*pgen2.d;
                 glm::vec3 var = glm::vec3(velVariance, velVariance, velVariance);
                 glm::vec3 vgenc = glm::gaussRand(vgen, var - glm::dot(var, vgen));
                 pgen2.pcpus[pgen2.pnum] = {
@@ -453,9 +467,32 @@ int main() {
                 velocity.x = sigma * (pgen1.pgpus[i].p.y - pgen1.pgpus[i].p.x);
                 velocity.y = pgen1.pgpus[i].p.x * (rho - pgen1.pgpus[i].p.z) - pgen1.pgpus[i].p.y;
                 velocity.z = pgen1.pgpus[i].p.x * pgen1.pgpus[i].p.y - beta * pgen1.pgpus[i].p.z;
+                glm::vec3 p_prev = pgen1.pgpus[i].p;
+                float d = glm::dot(pgen1.pgpus[i].p - tri[0], norm);
                 pgen1.pgpus[i].p += pgen1.pcpus[i].v * deltaTimeFrame;
-                pgen1.pcpus[i].v += g * glm::vec3(0.0,0.0,-1.0) * deltaTimeFrame;
-                pgen1.pcpus[i].v = (1 - lorenzFac*0.01f) * pgen1.pcpus[i].v + lorenzFac*0.01f * velocity;
+                float dn = glm::dot(pgen1.pgpus[i].p - tri[0], norm);
+                pgen1.pcpus[i].v += g * glm::vec3(0.0, 0.0, -1.0) * deltaTimeFrame;
+                pgen1.pcpus[i].v = (1 - lorenzFac * 0.01f) * pgen1.pcpus[i].v + lorenzFac * 0.01f * velocity;
+                if (signbit(d) != signbit(dn)) {
+                    printf("coll level 1\n");
+                    //collision may happened need to check projections
+                    glm::vec3 collP = p_prev + pgen1.pcpus[i].v * deltaTimeFrame * (abs(d) / (abs(d) + abs(dn)));
+                    //for xy
+                    float e1 = hpSign(glm::vec2(collP.y, collP.z), glm::vec2(tri[0].y, tri[0].z), glm::vec2(tri[1].y, tri[1].z));
+                    float e2 = hpSign(glm::vec2(collP.y, collP.z), glm::vec2(tri[1].y, tri[1].z), glm::vec2(tri[2].y, tri[2].z));
+                    float e3 = hpSign(glm::vec2(collP.y, collP.z), glm::vec2(tri[2].y, tri[2].z), glm::vec2(tri[0].y, tri[0].z));
+
+                    if (signbit(e1) == signbit(e2) && signbit(e2) == signbit(e3)) {
+                        printf("coll level 2\n");
+                        printf("coll happened\n");
+                        pgen1.pgpus[i].p -= 2.0f*glm::dot(pgen1.pgpus[i].p,norm)*norm;
+                        glm::vec3 vn = glm::dot(pgen1.pcpus[i].v, norm) * norm;
+
+                        glm::vec3 vt = pgen1.pcpus[i].v - vn;
+                        pgen1.pcpus[i].v = -vn+vt;
+                    }
+                }
+                
                 pgen1.pgpus[i].c =
                     (1 - (glm::length(pgen1.pcpus[i].v) / 100.0f)) * glm::vec3(.0, .0, 1.0) +
                     (glm::length(pgen1.pcpus[i].v) * glm::vec3(1.0, .0, .0) / 100.0f);
